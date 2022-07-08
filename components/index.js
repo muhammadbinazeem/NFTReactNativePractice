072396853b2946c047b8bf26a3f66e02b6d6f5db
muhammadbinazeem <57266096+muhammadbinazeem@users.noreply.github.com>
2022-07-08 20:22:27 +0500

    initail commit

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import FocusedStatusBar from "./FocusedStatusBar";
+import { CircleButton, RectButton } from "./Button";
+import { SubInfo } from "./Subinfo";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  SubInfo,
+  DetailsBid,
+  DetailsDesc,
+};
